08af84ee197804400480099a989b6bea706e23ef
Saikat <mukhopadhyay.s@husky.neu.edu>
2019-04-20 02:32:21 -0400

    Google Sign in : Not working

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
+import {Router} from '@angular/router';
 import {NgForm} from '@angular/forms';
 import {UserService} from '../services/user.service';
 import {CookieService} from 'ngx-cookie-service';
@@ -24,13 +24,13 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLogin(f: NgForm){
+  onLogin(f: NgForm) {
 
     this.userService.loginUser({
       username: f.value.username,
       password: f.value.password
     }).then((response) => {
-      if(response.success){
+      if (response.success) {
 
         this.userService.setSession(response);
         this.isLoginSuccessful = true;
@@ -41,7 +41,7 @@ export class LoginComponent implements OnInit {
         }, 1200);
 
         // this.router.navigate(['home']);
-      }else{
+      } else {
         this.isLoginUnsuccessful = true;
         this.errorMsg = response.message;
         setTimeout(() => {
@@ -49,6 +49,15 @@ export class LoginComponent implements OnInit {
         }, 3000);
       }
     });
+  }
+
+  onSignIn(googleUser) {
+    console.log("<---Here--->");
+    const profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
 
 }
